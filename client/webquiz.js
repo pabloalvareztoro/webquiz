9b2860e7950473cd9938ce7b9ff273330e807305
pabloalvareztoro <pabloalvareztoro@gmail.com>
2015-08-31 22:08:42 -0300

    Question retrieving from database

--- client/webquiz.js
+++ client/webquiz.js
@@ -1,20 +1,9 @@
-// counter starts at 0
-Session.setDefault('counter', 0);
 Questions = new Mongo.Collection("questions");
 
-
-Template.hello.helpers({
-  counter: function () {
-   return Session.get('counter');
-  },
-  questions: function(){
-    return Questions.find();
+Template.questionnaire.helpers({
+  question: function(){
+    return Questions.findOne({'id': 0});
   }
 });
 
-Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-  }
-});
+Meteor.subscribe('getQuestion');
--- server/webquiz.js
+++ server/webquiz.js
@@ -1,11 +1,14 @@
 Questions = new Mongo.Collection("questions");
 
 Meteor.startup(function () {
-  // code to run on server at startup
-   var defaultQuestions = JSON.parse(Assets.getText("json-documents/questions.json"));
-    if (Questions.find().count() == 0) {
-        for (var i=0; i<defaultQuestions.length; i++) {
-        Questions.insert(defaultQuestions[i]);
-       }
+  var defaultQuestions = JSON.parse(Assets.getText("json-documents/questions.json"));
+  if (Questions.find().count() == 0) {
+      for (var i=0; i<defaultQuestions.length; i++) {
+      Questions.insert(defaultQuestions[i]);
     }
+  }
+});
+
+Meteor.publish('getQuestion', function () {
+  return Questions.find({'id': 0});
 });
